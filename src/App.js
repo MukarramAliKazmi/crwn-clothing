e72f3edc68f83d0d822222743574ac707c9806e0
Mukarram Kazmi <muazzam.ali.kazmi@gmail.com>
2022-08-19 23:38:54 +0500

    Added components (directory and category-tiem)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ];
-
   return (
-    <div className='categories-container'>
-      {categories.map(({ id, title }) => (
-        <div key={id} className='category-container'>
-          {/* <img /> */}
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory />
   );
 };
 
