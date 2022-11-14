3963bc0ef686d2fb80655204a69362dbf4b34c67
Mukarram Kazmi <muazzam.ali.kazmi@gmail.com>
2022-11-14 12:00:12 +0500

    add: user context and signout feature

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,21 @@
+import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import { UserContext } from '../../contexts/user.context';
+
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss'
 
 const Nevigation = () => {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const signOUtHandler = async () => {
+        await signOutUser();
+        setCurrentUser(null);
+    }
+
     return (
         <>
             <div className='navigation'>
@@ -15,9 +26,17 @@ const Nevigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/sign-in'>
-                        SIGN IN
-                    </Link>
+                    {
+                        currentUser ? (
+                            <span className='nav-link' onClick={signOUtHandler}>
+                                SIGN OUT
+                            </span>
+                        ) : (
+                            <Link className='nav-link' to='/sign-in'>
+                                SIGN IN
+                            </Link>
+                        )
+                    }
                 </div>
             </div>
             <Outlet />
