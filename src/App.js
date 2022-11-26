1b2fa738da811aafd06948fbc5eff44615793e49
Mukarram Kazmi <muazzam.ali.kazmi@gmail.com>
2022-11-26 17:06:31 +0500

    added catogry, category-preview component, and finilize the project

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Nevigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path='shop/*' element={<Shop />} />
         <Route path='sign-in' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
