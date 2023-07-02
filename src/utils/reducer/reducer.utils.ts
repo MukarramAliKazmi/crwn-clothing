a0dfc40bb62e35f918c5499e6931cc61976c7222
Syed Mukarram Ali Kazmi <mukarram.ali.kazmi@gmail.com>
2023-07-02 18:49:22 +0500

    feat: Change categories in Redux to typescript

---
+++ src/utils/reducer/reducer.utils.ts
@@ -0,0 +1,47 @@
+import { AnyAction } from "redux";
+
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>["type"];
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCrator: AC
+): Matchable<AC>;
+
+export function withMatcher<
+  AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCrator: AC): Matchable<AC>;
+
+export function withMatcher(actionCrator: Function) {
+  const type = actionCrator().type;
+  return Object.assign(actionCrator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
+export type ActionWithPayload<T, P> = {
+  type: T;
+  payload: P;
+};
+
+export type Action<T> = {
+  type: T;
+};
+
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
+
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>;
+
+export function createAction<T extends string, P>(type: T, payload: P) {
+  return { type, payload };
+}
